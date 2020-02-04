eed2886c35057dccb7f79e3a0aee3ad8e43d2274
buppter <shixintian@aliyun.com>
2020-02-04 21:29:27 +0800

    :white_check_mark: update tests

--- test/test_curd.py
+++ test/test_curd.py
@@ -1,7 +1,9 @@
 import unittest
 import sys
 from flask import current_app
+
 sys.path.append("../")
+
 from app import create_app
 from app.models import db
 from app.models.organization import Node
@@ -34,38 +36,82 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(current_app.config['TESTING'])
 
     def test_retrieve_node_is_not_found(self):
-        res = self.client.get("/v1/org/1000").json
+        res = self.client.get("/v1/org/1000")
+        self.assertEqual(res.status_code, 404)
+        res = res.json
         self.assertEqual(res["code"], 404)
         self.assertEqual(res["data"], [])
 
     def test_retrieve_single_node(self):
-        res = self.client.get("v1/org/1").json
+        res = self.client.get("v1/org/1")
+        self.assertEqual(res.status_code, 200)
+        res = res.json
         self.assertEqual(res["code"], 200)
         self.assertIsInstance(res["data"], dict)
         self.assertTrue("name" in res["data"])
 
     def test_retrieve_all_nodes(self):
-        res = self.client.get("/v1/orgs").json
+        res = self.client.get("/v1/orgs")
+        self.assertEqual(res.status_code, 200)
+        res = res.json
         self.assertEqual(res["code"], 200)
         self.assertIsInstance(res["data"], list)
         self.assertTrue("descendant" in res["data"][0])
 
     def test_create_no_data(self):
-        res = self.client.post("/v1/orgs", json={}).json
-        self.assertEqual(res["code"], 400)
+        res = self.client.post("/v1/orgs", json={})
+        self.assertEqual(res.status_code, 415)
+        res = res.json
+        self.assertEqual(res["code"], 415)
 
     def test_create_bad_data(self):
-        res = self.client.post("/v1/orgs", json={"name": "test"}).json
+        res = self.client.post("/v1/orgs", json={"name": "test"})
+        self.assertEqual(res.status_code, 400)
+        res = res.json
         self.assertEqual(res["code"], 400)
 
     def test_create_not_exist_ancestor(self):
-        res = self.client.post("/v1/orgs", json={"name": "test", "ancestor": "test"}).json
+        res = self.client.post("/v1/orgs", json={"name": "test", "ancestor": "test"})
+        self.assertEqual(res.status_code, 400)
+        res = res.json
         self.assertEqual(res["code"], 400)
 
     def test_create_right_data(self):
-        res = self.client.post("/v1/orgs", json={"name": "test", "ancestor": "平台开发组"}).json
+        res = self.client.post("/v1/orgs", json={"name": "test", "ancestor": "平台开发组"})
+        self.assertEqual(res.status_code, 201)
+        res = res.json
         self.assertEqual(res["code"], 201)
         self.assertEqual(res["data"], [])
+
+    def test_update_no_data(self):
+        res = self.client.put("/v1/org/2", json={})
+        self.assertEqual(res.status_code, 415)
+        self.assertEqual(res.json["code"], 415)
+
+    def test_update_no_exist_ancestor(self):
+        res = self.client.put("/v1/org/2", json={"name": "test", "ancestor": "test22"})
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json['code'], 400)
+
+    def test_update_not_complete_data(self):
+        res = self.client.put("/v1/org/2", json={"name": "test"})
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json['code'], 400)
+
+    def test_update_right_data(self):
+        res = self.client.put("/v1/org/2", json={"name": "test2", "ancestor": "web开发组"})
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.json["code"], 200)
+
+    def test_delete_wrong_id(self):
+        res = self.client.delete("/v1/org/100")
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(res.json["code"], 404)
+
+    def test_delete_right_id(self):
+        res = self.client.delete("/v1/org/3")
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.json["code"], 200)
 
 
 if __name__ == '__main__':
