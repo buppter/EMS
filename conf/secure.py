44ead6c185a434e0a57f18105e53fa88690922ef
buppter <shixintian@aliyun.com>
2020-02-05 17:13:57 +0800

    :wrench: Changing configuration files.

--- conf/secure.py
+++ conf/secure.py
@@ -1,6 +1,6 @@
 SECRET_KEY = "employee management system"
 
-DEV_SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1:3306/ netease'
-TEST_SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1:3306/test'
+DEV_SQLALCHEMY_DATABASE_URI = 'mysql://root@127.0.0.1:3306/netease'
+TEST_SQLALCHEMY_DATABASE_URI = 'mysql://root@127.0.0.1:3306/test'
 
 PER_PAGE_NUM = 20
