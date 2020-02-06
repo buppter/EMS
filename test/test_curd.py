78ef079ede3a9dd632d6b66958a193fc6bf77643
buppter <shixintian@aliyun.com>
2020-02-06 16:54:46 +0800

    :white_check_mark: Adding tests.

--- test/test_curd.py
+++ test/test_curd.py
@@ -32,6 +32,11 @@ class MyTestCase(unittest.TestCase):
         db.session.add_all([node1, node2, node3])
         db.session.commit()
 
+    @staticmethod
+    def clean_redis_data():
+        from app.utils.redis_cli import Redis
+        Redis.delete("127.0.0.1")
+
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'])
 
@@ -41,6 +46,7 @@ class MyTestCase(unittest.TestCase):
         res = res.json
         self.assertEqual(res["code"], 404)
         self.assertEqual(res["data"], [])
+        self.clean_redis_data()
 
     def test_retrieve_single_node(self):
         res = self.client.get("v1/orgs/1")
@@ -49,6 +55,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(res["code"], 200)
         self.assertIsInstance(res["data"], dict)
         self.assertTrue("name" in res["data"])
+        self.clean_redis_data()
 
     def test_retrieve_all_nodes(self):
         res = self.client.get("/v1/orgs")
@@ -57,6 +64,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(res["code"], 200)
         self.assertIsInstance(res["data"], list)
         self.assertTrue("subs" in res["data"][0])
+        self.clean_redis_data()
 
     def test_create_no_data(self):
         res = self.client.post("/v1/orgs", json={})
@@ -64,6 +72,7 @@ class MyTestCase(unittest.TestCase):
         res = res.json
         self.assertEqual(res["code"], 415)
         self.assertTrue("json" in res["msg"])
+        self.clean_redis_data()
 
     def test_create_bad_data(self):
         res = self.client.post("/v1/orgs", json={"name": "test"})
@@ -71,6 +80,7 @@ class MyTestCase(unittest.TestCase):
         res = res.json
         self.assertEqual(res["code"], 400)
         self.assertTrue("不完整" in res["msg"])
+        self.clean_redis_data()
 
     def test_create_not_exist_ancestor(self):
         res = self.client.post("/v1/orgs", json={"name": "test", "ancestor": "test"})
@@ -78,6 +88,7 @@ class MyTestCase(unittest.TestCase):
         res = res.json
         self.assertEqual(res["code"], 400)
         self.assertTrue("不存在" in res['msg'])
+        self.clean_redis_data()
 
     def test_create_exist_org(self):
         res = self.client.post("v1/orgs", json={"name": "平台开发组", "ancestor": "伏羲实验室"})
@@ -85,6 +96,7 @@ class MyTestCase(unittest.TestCase):
         res = res.json
         self.assertEqual(res["code"], 400)
         self.assertTrue("已存在" in res["msg"])
+        self.clean_redis_data()
 
     def test_create_right_data(self):
         res = self.client.post("/v1/orgs", json={"name": "test", "ancestor": "平台开发组"})
@@ -93,81 +105,87 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(res["code"], 201)
         self.assertEqual(res["data"], [])
         self.assertTrue("created" in res["msg"])
+        self.clean_redis_data()
 
     def test_update_no_data(self):
         res = self.client.put("/v1/orgs/2", json={})
         self.assertEqual(res.status_code, 415)
         self.assertEqual(res.json["code"], 415)
         self.assertTrue("json" in res.json["msg"])
+        self.clean_redis_data()
 
     def test_update_no_exist_ancestor(self):
         res = self.client.put("/v1/orgs/2", json={"name": "test", "ancestor": "test22"})
         self.assertEqual(res.status_code, 400)
         self.assertEqual(res.json['code'], 400)
         self.assertTrue("不存在" in res.json["msg"])
+        self.clean_redis_data()
 
     def test_update_not_complete_data(self):
         res = self.client.put("/v1/orgs/2", json={"name": "test"})
         self.assertEqual(res.status_code, 400)
         self.assertEqual(res.json['code'], 400)
         self.assertTrue("不完整" in res.json["msg"])
+        self.clean_redis_data()
 
     def test_update_right_data(self):
         res = self.client.put("/v1/orgs/2", json={"name": "test2", "ancestor": "web开发组"})
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.json["code"], 200)
         self.assertTrue("success" in res.json["msg"])
+        self.clean_redis_data()
 
     def test_delete_wrong_id(self):
         res = self.client.delete("/v1/orgs/100")
         self.assertEqual(res.status_code, 404)
         self.assertEqual(res.json["code"], 404)
         self.assertTrue("not found" in res.json["msg"])
+        self.clean_redis_data()
 
     def test_delete_right_id(self):
         res = self.client.delete("/v1/orgs/3")
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.json["code"], 200)
         self.assertTrue("success" in res.json["msg"])
+        self.clean_redis_data()
 
     def test_get_exist_node_ancestor(self):
         res = self.client.get("/v1/orgs/ancestor/3")
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.json["code"], 200)
         self.assertTrue("id" in res.json["data"])
+        self.clean_redis_data()
 
     def test_get_not_exist_node_ancestor(self):
         res = self.client.get("/v1/orgs/ancestor/100")
         self.assertEqual(res.status_code, 404)
         self.assertEqual(res.json["code"], 404)
+        self.clean_redis_data()
 
     def test_get_exist_node_sub(self):
         res = self.client.get("/v1/orgs/subs/1")
         self.assertEqual(res.status_code, 200)
         self.assertIsInstance(res.json["data"], list)
+        self.clean_redis_data()
 
     def test_get_not_exist_node_sub(self):
         res = self.client.get("/v1/orgs/subs/100")
         self.assertEqual(res.status_code, 404)
         self.assertEqual(res.json["code"], 404)
+        self.clean_redis_data()
 
     def test_get_exist_node_sub_with_queries(self):
         res = self.client.get("/v1/orgs/subs/3?page=1&per_page=1&limit=1&offset=1")
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.json["code"], 200)
         self.assertIsInstance(res.json["data"], list)
-
-    def test_limit_rate(self):
-        for _ in range(11):
-            res = self.client.get("/v1/orgs")
-        self.assertEqual(res.status_code, 403)
-        self.assertEqual(res.json["code"], 403)
-        self.assertTrue("limit" in res.json["msg"])
+        self.clean_redis_data()
 
     def test_405(self):
         res = self.client.post("/v1/orgs/1", json={})
         self.assertEqual(res.status_code, 405)
         self.assertEqual(res.json["code"], 405)
+        self.clean_redis_data()
 
     def test_500(self):
         import os
@@ -175,6 +193,16 @@ class MyTestCase(unittest.TestCase):
         res = self.client.put("/v1/orgs/2", json={"name": "test2", "ancestor": "web开发组"})
         self.assertEqual(res.status_code, 500)
         self.assertEqual(res.json["code"], 500)
+        os.system("sudo systemctl start mysql")
+
+
+    def test_limit_rate(self):
+        for _ in range(11):
+            res = self.client.get("/v1/orgs")
+        self.assertEqual(res.status_code, 403)
+        self.assertEqual(res.json["code"], 403)
+        self.assertTrue("limit" in res.json["msg"])
+        self.clean_redis_data()
 
 
 if __name__ == '__main__':
