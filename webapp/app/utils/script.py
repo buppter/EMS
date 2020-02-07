0644166a3d05462dde54b314e2a60f2d43d69f7d
buppter <shixintian@aliyun.com>
2020-02-07 19:50:54 +0800

    :sparkles: 增加Flask_Migrate和Flask_Script

---
+++ webapp/app/utils/script.py
@@ -0,0 +1,42 @@
+from flask_script import Command, Option
+
+
+class Insert(Command):
+    """
+    insert data to database
+    """
+
+    def __init__(self, default_times=20):
+        self.default_times = default_times
+
+    def get_options(self):
+        return [
+            Option('-t', '--times', dest="times", default=self.default_times),
+        ]
+
+    def run(self, times):
+        from app.models.insert_data import insert_employee_data, insert_org_data
+        try:
+            times = int(times)
+        except ValueError:
+            times = self.default_times
+        insert_org_data()
+        insert_employee_data(times)
+
+
+class Create(Command):
+    """create all tables"""
+
+    def run(self):
+        from app.models import db
+        from app.models.organization import Node
+        from app.models.employee import Employee
+        db.create_all()
+
+
+class Drop(Command):
+    """drop all tables"""
+
+    def run(self):
+        from app.models import db
+        db.drop_all()
--- webapp/manager.py
+++ webapp/manager.py
@@ -2,16 +2,16 @@ from app import create_app
 from flask_migrate import MigrateCommand
 from flask_script import Manager
 
+from app.utils.script import Insert, Create, Drop
+
 app = create_app()
+
 manager = Manager(app)
+
 manager.add_command("db", MigrateCommand)
-
-
-@manager.command
-def create():
-    from app.models import db
-    db.create_all()
-
+manager.add_command("insert", Insert())
+manager.add_command("create", Create())
+manager.add_command("drop", Drop())
 
 if __name__ == "__main__":
     manager.run()
