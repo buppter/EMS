78fd028646596cea0fda04e9bc671d4c2db69dd9
buppter <shixintian@aliyun.com>
2020-02-07 14:47:26 +0800

    :sparkles: 添加新数据库创建命令

--- webapp/manager.py
+++ webapp/manager.py
@@ -7,5 +7,11 @@ manager = Manager(app)
 manager.add_command("db", MigrateCommand)
 
 
+@manager.command
+def create():
+    from app.models import db
+    db.create_all()
+
+
 if __name__ == "__main__":
     manager.run()
