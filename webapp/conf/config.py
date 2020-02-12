aa62245edf658d5c8f22c6d486b5623612b049e1
buppter <shixintian@aliyun.com>
2020-02-12 13:40:34 +0800

    :wrench: Changing configuration files.

--- webapp/conf/config.py
+++ webapp/conf/config.py
@@ -7,6 +7,7 @@ class Config:
     SECRET_KEY = secure.SECRET_KEY
     JSON_SORT_KEYS = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    APP_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
 class DevelopmentConfig(Config):
@@ -14,21 +15,18 @@ class DevelopmentConfig(Config):
     # SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = secure.DEV_SQLALCHEMY_DATABASE_URI
     REDIS_URL = secure.DEV_REDIS_URL
-    APP_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = secure.TEST_SQLALCHEMY_DATABASE_URI
     REDIS_URL = secure.TEST_REDIS_URL
-    APP_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = secure.PROD__SQLALCHEMY_DATABASE_URI
     REDIS_URL = secure.PROD_REDIS_URL
-    APP_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
 config = {
